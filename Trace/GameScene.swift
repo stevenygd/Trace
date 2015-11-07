41d11c0c5cb1e5ab2e0a8e9ed991e7e281011a60
Grendel Yang <stevenygd@gmail.com>
2015-11-07 04:31:23 -0500

    [add] add the draw a ling thing

--- Trace/GameScene.swift
+++ Trace/GameScene.swift
@@ -9,36 +9,70 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let screenWith   = UIScreen.mainScreen().bounds.size.width
+    let screenHeight = UIScreen.mainScreen().bounds.size.height
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 45;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
         self.addChild(myLabel)
+        
+        // load game data
+        if let path = NSBundle.mainBundle().pathForResource("scene", ofType:"dat") {
+            if let data:NSData = NSFileManager.defaultManager().contentsAtPath(path) {
+                do{
+                    let parsedObject: AnyObject? = try NSJSONSerialization.JSONObjectWithData(data,
+                        options: NSJSONReadingOptions.AllowFragments)
+                    print(parsedObject);
+                } catch is NSError {
+                    print("Something is wrong");
+                }
+            }else{
+                print("GOT NOTHING");
+            }
+        }else{
+            print("get no path");
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
         for touch in touches {
-            let location = touch.locationInNode(self)
+
+            let startPoint  : CGPoint = CGPointMake(10, 10);
+            let endPoint    : CGPoint = CGPointMake(0.5, 0.5);
+            let yourline: SKShapeNode = drawLine(startPoint, e: endPoint);
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            yourline.xScale = 1
+            yourline.yScale = 1
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            yourline.position = touch.locationInNode(self)
+
+            NSLog("%0.2f, %0.2f", touch.locationInView(self.view).x, touch.locationInView(self.view).y)
+            NSLog("%0.2f, %0.2f", touch.locationInNode(self).x, touch.locationInNode(self).y)
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            self.addChild(yourline);
         }
     }
    
+    /**
+     * [pre] s, e lays inside [0,0]<->[1,1]
+     */
+    private func drawLine(s: CGPoint, e: CGPoint) -> SKShapeNode {
+        let line: SKShapeNode = SKShapeNode();
+        let pathToDraw : CGMutablePathRef = CGPathCreateMutable();
+        CGPathMoveToPoint(pathToDraw, nil, s.x * screenWith, s.y * screenHeight)
+        CGPathAddLineToPoint(pathToDraw, nil, e.x * screenWith, e.y * screenHeight);
+        line.path = pathToDraw;
+
+        return line;
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
